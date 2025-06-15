c3e4a944c90c39f395dd9146a5f7f29adaca9847
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2025-06-15 20:31:30 +0100

    return users

--- src/config.js
+++ src/config.js
@@ -25,5 +25,5 @@ const Loginschema = new mongoose.Schema({
     }
 });
 
-const collection = mongoose.model("User", Loginschema, "users");
+const collection = mongoose.model("users", Loginschema);
 module.exports = collection;
