559ac90f90f727039197e3c2dc58ced6bc41d690
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2025-06-16 20:39:21 +0100

    other upadate config.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const path = require("path");
-const collection = require("../src/config"); // ✅ Correto
+const collection = require("./config"); // ✅ Correto
 const bcrypt = require('bcrypt');
 
 const app = express();
