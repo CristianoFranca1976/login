1a952f470693bbbfc7e4fb0e45c18682c5e9f39a
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2025-06-22 21:50:57 +0100

    try fix 1

--- src/index.js
+++ src/index.js
@@ -14,6 +14,8 @@ app.use(express.static("image"));
 app.use(express.urlencoded({ extended: false }));
 //use EJS as the view engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
 
 app.use(
   session({
